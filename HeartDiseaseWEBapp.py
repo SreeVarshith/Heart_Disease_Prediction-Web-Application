b389a50d0c626f4fe637b8d225e832adeca329dc
Satla Sree Varshith <95604448+SreeVarshith@users.noreply.github.com>
2024-02-24 21:53:50 +0530

    Update HeartDiseaseWEBapp.py

--- HeartDiseaseWEBapp.py
+++ HeartDiseaseWEBapp.py
@@ -1,41 +1,42 @@
-import os
 import numpy as np
 import pickle
 import streamlit as st
+from io import BytesIO
+import requests
 
-# Get the directory of the script
-script_dir = os.path.dirname(__file__)
-file_path = os.path.join(script_dir, 'trained_model.sav')
+# Specify the raw URL of the trained_model.sav file on GitHub
+github_raw_url = 'https://raw.githubusercontent.com/SreeVarshith/Heart_Disease_Prediction-Web-Application/main/Heart_Disease_Prediction-Web-Application/trained_model.sav'
 
-# Check if the file exists before attempting to open
-if os.path.exists(file_path):
+# Load the model from GitHub using st.cache to ensure it's loaded only once
+@st.cache(allow_output_mutation=True)
+def load_model():
     try:
-        loaded_model = pickle.load(open(file_path, 'rb'))
+        response = requests.get(github_raw_url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+
+        return pickle.load(BytesIO(response.content))
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error accessing GitHub URL: {e}")
+        return None
     except Exception as e:
         st.error(f"Error loading the model: {e}")
-else:
-    st.error(f"Error: File '{file_path}' not found.")
+        return None
+
+loaded_model = load_model()
 
 def heart_disease_prediction(input):
-    # Convert empty strings to NaN (Not a Number)
-    input = [np.nan if value == '' else value for value in input]
-
-    try:
-        in_np = np.asarray(input, dtype=float)
-    except ValueError as ve:
-        st.error(f"Error converting input to NumPy array: {ve}")
+    if loaded_model is None:
+        st.error("Model not loaded. Please check the GitHub URL.")
         return
 
+    in_np = np.asarray(input, dtype=float)
     np_reshape = in_np.reshape(1, -1)
     prediction = loaded_model.predict(np_reshape)
-
-    print("Prediction:", prediction)
 
     if prediction[0] == 0:
         return "The person does not have heart disease"
     else:
         return "The person has heart disease"
-
 
 def main():
     st.title('Heart Disease Prediction Web App')
@@ -57,8 +58,7 @@ def main():
     prediction = ''
 
     if st.button('Heart disease Prediction'):
-        input_values = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
-        prediction = heart_disease_prediction(input_values)
+        prediction = heart_disease_prediction([age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal])
 
     st.success(prediction)
 
